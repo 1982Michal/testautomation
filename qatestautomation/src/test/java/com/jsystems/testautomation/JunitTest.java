93365002fc3d4363b3554460b2c22c6db633b4f7
michal <michalvitava@wp.pl>
2018-05-22 10:37:52 +0200

    create firstTest, create and set pom.xml

--- qatestautomation/src/test/java/com/jsystems/testautomation/JunitTest.java
+++ qatestautomation/src/test/java/com/jsystems/testautomation/JunitTest.java
@@ -2,10 +2,13 @@ package com.jsystems.testautomation;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.math.BigDecimal;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
 public class JunitTest {
+
 @Test
 public void firstTest() {
 
@@ -13,6 +16,25 @@ public void firstTest() {
 
     assertTrue(true == true);
     assertTrue(testowyString.equals("firstTest"));
+    assertTrue("messege from assertTrue", 5 == 2 +3);
+    assertFalse("messege from assertTrue", 5 == 3 +3);
+    assertFalse("messege from assertTrue", testowyString.equals("Adam"));
+    assertThat(testowyString, is("firstTest"));
+    assertEquals(testowyString, "firstTest");
+    assertSame(testowyString, "firstTest");
+    assertThat(testowyString, containsString("first"));
+    assertThat(testowyString, equalTo("firstTest"));
+    assertThat(testowyString, endsWith("Test"));
+
+    System.out.println(0.2 * 0.2);
+
+    assertFalse(0.2 * 0.2 == 0.04);
+    assertTrue(new BigDecimal("0.2").multiply(new BigDecimal("0.2")).doubleValue() == 0.04);
+
+
+
+
+
 }
 
 
